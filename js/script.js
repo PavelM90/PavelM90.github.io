ed02d377049373b67d7ae53ad1e04651149ba124
Pavel Makarov <121988510+PavelM90@users.noreply.github.com>
2024-07-20 08:40:05 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,18 +2,28 @@ document.addEventListener('DOMContentLoaded', () => {
     const content = document.getElementById('content');
 
     async function fetchMarkdown(file) {
-        const response = await fetch(file);
-        if (!response.ok) {
-            content.innerHTML = '<p>Content not found.</p>';
+        try {
+            const response = await fetch(file);
+            if (!response.ok) {
+                content.innerHTML = '<p>Content not found.</p>';
+                console.error('Error fetching the file:', response.statusText);
+                return '';
+            }
+            const text = await response.text();
+            return marked(text);
+        } catch (error) {
+            console.error('Error fetching the file:', error);
+            content.innerHTML = '<p>Error loading content.</p>';
             return '';
         }
-        const text = await response.text();
-        return marked(text);
     }
 
     async function loadContent() {
         let hash = window.location.hash || '#about';
         let file = `assets/md/${hash.substring(1)}.md`;
+
+        console.log('Loading content for:', hash);
+        console.log('Fetching file:', file);
 
         const markdown = await fetchMarkdown(file);
         content.innerHTML = markdown;
